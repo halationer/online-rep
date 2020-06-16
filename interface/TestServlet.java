2b25ffe7edbdfe4d5eed638a61febe461093fd4c
halation <1124792103@qq.com>
2020-06-16 12:17:14 +0800

    del a hello

---
+++ interface/TestServlet.java
@@ -0,0 +1,54 @@
+package web;
+
+import java.io.IOException;
+import java.net.URLEncoder;
+import java.util.*;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import dao.SqlVisiter;
+
+/**
+ * Servlet implementation class TestServlet
+ */
+@WebServlet("/TestServlet")
+public class TestServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public TestServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		List<Student> students = SqlVisiter.executeQuery(
+			"select * from students",
+			Student.class
+		);
+		System.out.println(request.getParameter("haha"));
+		request.getSession().setAttribute("studentname", students.get(0).getName());
+//		request.getRequestDispatcher("testmain.jsp").forward(request, response);
+		//解决从servlet到jsp页面中文乱码问题：URLEncoder.encode
+		response.sendRedirect("testmain.jsp?studentname="+URLEncoder.encode(students.get(0).getName(),"utf-8"));
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
